f3c64c70342c200b553f8a0569388d54a3bbc04e
Caitlyn Griffing <caitlyn.griffing@gmail.com>
2021-12-11 17:26:35 -0600

    added modal for password preferences

--- styles/script.js
+++ styles/script.js
@@ -1,9 +1,43 @@
+const modal = document.getElementById("myModal");
+
+const openModal = document.getElementById("open");
+
+const closeModal = document.getElementById("close");
+
+openModal.onclick = function() {
+  modal.style.display = "block";
+}
+
+closeModal.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
+// // MODAL //
+// $('.open').on("click", function() {
+//   $(".container, .settings").addClass("active"); 
+// });
+
+// // close modal //
+// $(".close, .container").on("click", function() {
+//   $(".container, .settings").removeClass("active");
+// });
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -12,3 +46,22 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// let pwLength = prompt('Choose the length of your password', length);
+// if (pwLength) {
+//   //between how ever many letters then 
+// }
+// let checkedSettings = upperCase, lowerCase, symbol, number;
+// let upperCase = confirm('Include uppercase?');
+// if (upperCase) {
+//   //add into password generator
+// } else {
+//   //don't add capital letters to password
+// }
+// let lowerCase = confirm('Include lowercase?');
+// let number = confirm('Include numbers?');
+// let symbol = confirm('Include symbols?');
+
+
+// let length = ;
+
